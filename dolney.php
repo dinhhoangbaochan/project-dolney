083d2f288902b64ad0f910ee4e1a6e9287f9e206
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-18 21:26:00 +0700

    copy basic main file from elementor

--- dolney.php
+++ dolney.php
@@ -6,3 +6,81 @@
  * Author: ChanDinh.com
  * Version: 0.0.1
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'ELEMENTOR_VERSION', '0.0.1' );
+
+define( 'ELEMENTOR__FILE__', __FILE__ ); // this point to this dolney.php file
+define( 'ELEMENTOR_PLUGIN_BASE', plugin_basename( ELEMENTOR__FILE__ ) );
+define( 'ELEMENTOR_PATH', plugin_dir_path( ELEMENTOR__FILE__ ) ); // this returns the path to plugin directory
+
+if ( defined( 'ELEMENTOR_TESTS' ) && ELEMENTOR_TESTS ) {
+	define( 'ELEMENTOR_URL', 'file://' . ELEMENTOR_PATH );
+} else {
+	define( 'ELEMENTOR_URL', plugins_url( '/', ELEMENTOR__FILE__ ) );
+}
+
+define( 'ELEMENTOR_MODULES_PATH', plugin_dir_path( ELEMENTOR__FILE__ ) . '/modules' );
+define( 'ELEMENTOR_ASSETS_PATH', ELEMENTOR_PATH . 'assets/' );
+define( 'ELEMENTOR_ASSETS_URL', ELEMENTOR_URL . 'assets/' );
+
+// Display notice if PHP is lower than 7.4 or WordPress is not 6.0
+if ( ! version_compare( PHP_VERSION, '7.4', '>=' ) ) {
+	add_action( 'admin_notices', 'elementor_fail_php_version' );
+} elseif ( ! version_compare( get_bloginfo( 'version' ), '6.0', '>=' ) ) {
+	add_action( 'admin_notices', 'elementor_fail_wp_version' );
+} else {
+  // Otherwise, call plugin.php in includes
+	require ELEMENTOR_PATH . 'includes/plugin.php';
+}
+
+/**
+ * Elementor admin notice for minimum PHP version.
+ *
+ * Warning when the site doesn't have the minimum required PHP version.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function elementor_fail_php_version() {
+	$html_message = sprintf(
+		'<div class="error"><h3>%1$s</h3><p>%2$s <a href="https://go.elementor.com/wp-dash-update-php/" target="_blank">%3$s</a></p></div>',
+		esc_html__( 'Elementor isn’t running because PHP is outdated.', 'elementor' ),
+		sprintf(
+			/* translators: %s: PHP version. */
+			esc_html__( 'Update to version %s and get back to creating!', 'elementor' ),
+			'7.4'
+		),
+		esc_html__( 'Show me how', 'elementor' )
+	);
+
+	echo wp_kses_post( $html_message );
+}
+
+/**
+ * Elementor admin notice for minimum WordPress version.
+ *
+ * Warning when the site doesn't have the minimum required WordPress version.
+ *
+ * @since 1.5.0
+ *
+ * @return void
+ */
+function elementor_fail_wp_version() {
+	$html_message = sprintf(
+		'<div class="error"><h3>%1$s</h3><p>%2$s <a href="https://go.elementor.com/wp-dash-update-wordpress/" target="_blank">%3$s</a></p></div>',
+		esc_html__( 'Elementor isn’t running because WordPress is outdated.', 'elementor' ),
+		sprintf(
+			/* translators: %s: WordPress version. */
+			esc_html__( 'Update to version %s and get back to creating!', 'elementor' ),
+			'6.0'
+		),
+		esc_html__( 'Show me how', 'elementor' )
+	);
+
+	echo wp_kses_post( $html_message );
+}
