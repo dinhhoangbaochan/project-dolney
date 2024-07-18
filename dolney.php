1ceb499ea54d2efaba8028a078cf61d61a7c81bb
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-07-18 21:04:23 +0700

    add main fields for plugin header

---
+++ dolney.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Dolney
+ * Description: A page builder, or at least what it tries to be.
+ * Plugin URL: https://chandinh.com
+ * Author: ChanDinh.com
+ * Version: 0.0.1
+ */
